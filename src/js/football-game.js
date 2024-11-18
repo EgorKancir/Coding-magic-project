9b5954bb21babfb80642bbf110d6bf030c627adf
EgorKancir <kancyregor@gmail.com>
2024-11-18 19:47:01 +0100

    Fix football

--- src/js/football-game.js
+++ src/js/football-game.js
@@ -1,7 +1,7 @@
 const ball = document.querySelector(".football-game__ball");
 const footballField = document.querySelector(".football-game");
 let clickMouse = false;
-let ballX, ballY, ballPosition;
+let ballX, ballY;
 ball.addEventListener("mousedown", (e) => {
     clickMouse = true;
     const rect = ball.getBoundingClientRect();
@@ -16,6 +16,7 @@ document.addEventListener('mousemove', (e) => {
         let newTop = e.clientY - ballY;
         newLeft = Math.max(parentRect.left, Math.min(newLeft, parentRect.right - elementRect.width));
         newTop = Math.max(parentRect.top, Math.min(newTop, parentRect.bottom - elementRect.height));
+        ball.style.transform = 'none';
         ball.style.left = `${newLeft - parentRect.left}px`;
         ball.style.top = `${newTop - parentRect.top}px`;
     }
